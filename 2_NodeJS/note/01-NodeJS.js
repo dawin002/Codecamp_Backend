b4695683007febd206954c9a9db0dcdba4a9d779
Dawin <dawin002@naver.com>
2023-12-23 19:36:26 +0900

    study(nodejs): add class note

---
+++ 2_NodeJS/note/01-NodeJS.js
@@ -0,0 +1,28 @@
+// Node.js
+
+//      JavaScript는 기본적으로 브라우저와 함께 동작하는 언어
+//      로컬 환경에서도 JavaScript 언어를 실행하기 위한 실행 프로그램
+
+// NPM
+
+//      Node Package Manager
+//      Node.js 기반에서 실행될 수 있는 모듈을 관리하는 관리자
+//      다른 사람들이 개발한 모듈, 라이브러리를 다운받거나 내가 만든 모듈, 라이브러리를 업로드할 수 있음
+//      https://www.npmjs.com
+
+//      NPM 사용법
+//          VScode에 NPM 설치해 사용 (node.js 설치시 자동 설치)
+//          코드 업로드 명령어: npm publish
+//          코드 다운로드 명령어: npm install
+
+// yarn
+
+//      페이스북이 자체 개발한 JavaScript 패키지 매니저
+//      npm과 동일한 역할
+//      성능(속도)과 보안 이슈를 보완
+
+//      yarn 사용법
+//          설치 명령어: npm install -g yarn 
+//                    (-g : global, 컴퓨터 전체에 설치)
+//          코드 업로드 명령어: 
+//          코드 다운로드 명령어: yarn add
