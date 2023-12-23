9bc7823a7a64d363701c2d3ca6bdf67bf994d28d
Dawin <dawin002@naver.com>
2023-12-23 17:59:19 +0900

    refactor(Precamp): 프리캠프 공부 폴더 이동

--- Precamp/ToDo-list/script.js
+++ Precamp/ToDo-list/script.js
@@ -2,7 +2,10 @@ const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 
 // 로컬 스토리지에서 saveItems 불러와 JSON 타입에서 원본 타입으로 되돌리기
-const savedTodoList = JSON.parse(localStorage.getItem('save-items'));
+const savedTodoList = JSON.parse(localStorage.getItem("save-items"));
+
+// 로컬 스토리지에서 날씨 데이터 불러오기
+const savedWeatherData = JSON.parse(localStorage.getItem("saved-weather"));
 
 // todo 하나 추가하는 함수
 const createTodo = function (storageData) {
@@ -14,21 +17,22 @@ const createTodo = function (storageData) {
         todoContents = storageData.contents;
     }
 
-    const newLi = document.createElement('li' );
-    const newSpan = document.createElement('span');
-    const newBtn = document.createElement('button');
+    const newLi = document.createElement("li");
+    const newSpan = document.createElement("span");
+    const newBtn = document.createElement("button");
 
     // 버튼에 이벤트 리스너 추가 (클릭 이벤트)
-    newBtn.addEventListener('click', () => {
+    newBtn.addEventListener("click", () => {
         // complete 라는 클래스 속성 추가
-        newLi.classList.toggle('complete');
+        newLi.classList.toggle("complete");
         //      toggle로 추가하는 거라 한번 누르면 추가 다시 한번 누르면 삭제됨
         // 버튼 눌릴 때마다 로컬 스토리지 저장
         saveItemsFunc();
     });
 
     // li 태그에 이벤트 리스너 추가 (더블 클릭 이벤트)
-    newLi.addEventListener('dblclick', () => {  // dblclick : 더블 클릭
+    newLi.addEventListener("dblclick", () => {
+        // dblclick : 더블 클릭
         // 이 태그 객체를 삭제
         newLi.remove();
         // 삭제될 때마다 로컬 스토리지 저장
@@ -38,19 +42,19 @@ const createTodo = function (storageData) {
     // 매개변수가 있고, 매개변수의 complete 값이 true 인 경우
     if (storageData?.complete) {
         // complete 라는 클래스 속성 추가
-        newLi.classList.add('complete');
+        newLi.classList.add("complete");
     }
     // 조건식 안 객채 뒤의 '?'
     //    : optional chaining
     //      객체가 undefined 이거나 null 이면 뒤쪽 코드 실행 안함
     //      if (storageData && storageData.complete) 조건식과 같음
     //      storageData 가 존재할 때만 .complete 실행
-    
+
     newSpan.textContent = todoContents;
     newLi.appendChild(newBtn);
     newLi.appendChild(newSpan);
     todoList.appendChild(newLi);
-    todoInput.value = '';
+    todoInput.value = "";
     saveItemsFunc();
 };
 
@@ -64,8 +68,8 @@ const keyCodeCheck = function () {
 
 // 모든 태그 삭제 함수
 const deleteAll = function () {
-    const liList = document.querySelectorAll('li');
-    for(li of liList) {
+    const liList = document.querySelectorAll("li");
+    for (li of liList) {
         li.remove();
         saveItemsFunc();
     }
@@ -74,81 +78,119 @@ const deleteAll = function () {
 // 로컬 스토리지에 저장 함수
 const saveItemsFunc = function () {
     const saveItems = [];
-    
-    for(let i=0; i<todoList.children.length; i++) {
+
+    for (let i = 0; i < todoList.children.length; i++) {
         const todoObj = {
             // i번째 li 태그 안의 span 태그의 텍스트
-            contents : todoList.children[i].querySelector('span').textContent,
+            contents: todoList.children[i].querySelector("span").textContent,
             // i번째 li 태그의 클래스 리스트의 complete 클래스 존재 여부
-            complete : todoList.children[i].classList.contains('complete'),
+            complete: todoList.children[i].classList.contains("complete"),
         };
         saveItems.push(todoObj);
-    };
+    }
 
     // JSON : 텍스트형 데이터 포맷 (객체나 데이터를 문자열로 바꿀 수 있음)
     // console.log(JSON.stringify(saveItems))
 
     // 배열이 비어있을 때는 로컬 스토리지에서 데이터 삭제(불필요한 데이터 삭제)
     if (saveItems.length === 0) {
-        localStorage.removeItem('save-items');
+        localStorage.removeItem("save-items");
     } else {
         // 로컬 스토리지에 JSON 형식으로 saveItems 배열 저장
-        localStorage.setItem('save-items', JSON.stringify(saveItems));
+        localStorage.setItem("save-items", JSON.stringify(saveItems));
     }
 
     // 바로 위 코드 3항 연산자로 대체 가능
-    // saveItems.length === 0 
-    //     ? localStorage.removeItem('save-items') 
+    // saveItems.length === 0
+    //     ? localStorage.removeItem('save-items')
     //     : localStorage.setItem('save-items', JSON.stringify(saveItems));
 };
 
 // 로컬 스토리지에 저장된 값이 있다면
 if (savedTodoList) {
     // 각 데이터로 todo 추가 함수 호출
-    for (let i = 0; i < savedTodoList.length; i ++) {
+    for (let i = 0; i < savedTodoList.length; i++) {
         createTodo(savedTodoList[i]);
     }
 }
 // 이 코드가 가장 아래에 내려와야 하는 이유
 // : createTodo() 함수가 정의된 후에 호출되어야 하기 때문
 
+/* 날씨 데이터 활용 */
+
+const weatherDataActive = function ({ location, weather }) {
+    // ToDo 제목을 현재 사용자 지역명으로 바꾸기
+    const locationNameTag = document.querySelector("#location-name-tag");
+    locationNameTag.textContent = location;
+
+    // 배경 이미지 날씨에 맞게 바꾸기
+    const weatherMainList = [
+        "Clear",
+        "Clouds",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Thunderstorm",
+    ];
+
+    weather = weatherMainList.includes(weather) ? weather : "Fog";
+
+    document.body.style.backgroundImage = `url('./images/${weather}.jpg')`;
+
+    // 로컬 스토리지에 날씨 데이터 저장하기
+    if (
+        !savedWeatherData ||
+        savedWeatherData.location !== location ||
+        savedWeatherData.weather !== weather
+    ) {
+        localStorage.setItem(
+            "saved-weather",
+            JSON.stringify(location, weather)
+        );
+    }
+};
 
 /* Web Weather Map API 사용해 날씨 정보 가져오기 */
 
-const weatherSearch = function({latitude, longitude}) {
+const weatherSearch = function ({ latitude, longitude }) {
     // 전달받는 위치 정보 객체를 구조분해할당해 사용
-    let key = '3b27ee52a6e2c2453328d0db9f28269c';
+    let key = "3b27ee52a6e2c2453328d0db9f28269c";
     fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
-    ) 
-    // then()으로 promise 객체가 fulfilled 상태가 되었을 때 처리해줌
-    .then((res) => {
-        // JSON 타입의 응답을 원래의 데이터 타입으로 변환해서 반환
-        // 헤더가 존재하는 응답이라 json() 사용
-        return res.json();  // json()이 promise 객체 반환
-    }) 
-    // 한번 더 then()으로 res.json()의 변환 작업이 완료되는 것을 기다렸다가 처리
-    .then((json) => {
-        console.log(json.name, json.weather[0].main);
-    })
-    .catch((err) => {
-        console.error(err);
-    });
-}
+    )
+        // then()으로 promise 객체가 fulfilled 상태가 되었을 때 처리해줌
+        .then((res) => {
+            // JSON 타입의 응답을 원래의 데이터 타입으로 변환해서 반환
+            // 헤더가 존재하는 응답이라 json() 사용
+            return res.json(); // json()이 promise 객체 반환
+        })
+        // 한번 더 then()으로 res.json()의 변환 작업이 완료되는 것을 기다렸다가 처리
+        .then((json) => {
+            // 필요한 데이터 객체로 만들기
+            const weatherData = {
+                location: json.name,
+                weather: json.weather[0].main,
+            };
+            weatherDataActive(weatherData);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+};
 
 /* Geolocation API 사용해 위치 정보 가져오기 */
 
 // 위치 접근에 성공했을 때 실행되는 함수
-const accessToGeo = function({ coords }) {
+const accessToGeo = function ({ coords }) {
     // 매개변수로 받은 현재 위치 객체를 구조분해할당해 coords만 받아옴
 
     // coords를 구조분해할당 -> latitude, longitude 선언
-    const {latitude, longitude} = coords;
+    const { latitude, longitude } = coords;
 
     // 위치 정보를 담는 객체
     const positionOBJ = {
         // shorthand property로 프로퍼티 초기화 생략
-        latitude,  // 위도
+        latitude, // 위도
         longitude, // 경도
     };
 
@@ -156,12 +198,16 @@ const accessToGeo = function({ coords }) {
 };
 
 // 위치 접근에 실패했을 때 실행되는 함수
-const err = function(err) {
+const err = function (err) {
     console.log(err);
 };
 
-const askForLocation = function() {
+const askForLocation = function () {
     navigator.geolocation.getCurrentPosition(accessToGeo, err);
 };
 
 askForLocation();
+
+if (savedWeatherData) {
+    weatherDataActive(savedWeatherData);
+}
