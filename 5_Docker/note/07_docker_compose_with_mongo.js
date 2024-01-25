62fbd3a488e39d22a92dbf9d5997a254f90b0f20
Dawin <dawin002@naver.com>
2024-01-26 03:40:45 +0900

    study(nodejs): docker-compose with mongodb

---
+++ 5_Docker/note/07_docker_compose_with_mongo.js
@@ -0,0 +1,130 @@
+// Docker compose
+
+//      MongoDB를 Docker와 함께 사용하려면 두 대의 Docker 컴퓨터가 필요함
+//          Express 서버를 실행하는 Docker 컴퓨터 한 대
+//          MongoDB 서버를 실행하는 Docker 컴퓨터 한 대
+
+//      Docker 컴퓨터 여러대 실행시킬 때는 Docker 들을 그룹핑해서 실행할 수 있음
+//      Docker 들을 그룹핑(연결)하는 것이 Docker compose
+
+// Docker MongoDB
+
+//      내 컴퓨터에 설치한 MongoDB는 사용 안함
+//      Docker 내부에 MongoDB 설치해 데이터베이스 서버 사용
+
+// 내 컴퓨터에 실행중인 MongoDB 종료하기
+
+//      Mac
+
+//      1. MongoDB 종료
+//          1) 터미널에 MongoDB 종료 명령어 입력
+                `brew services stop mongodb-community`
+
+//      2. MongoDB가 종료되었는지 확인
+//          1) brew 서비스 목록 조회 명령어 입력
+                `brew services`
+//          2) mongodb-community 의 Status 가 none 이 맞는지 확인
+
+//      Windows
+
+//      1.  MongoDB 켜져있는지 확인
+//          1) PowerShell 에서 MongoDB Shell 실행되는지 확인
+                `mongosh`
+//          2) 정상 실행되면 MongoDB 가 켜져있는 것이므로 아래 순서대로 종료하기
+
+//      2. MongoDB Shell 열어서 MongoDB 종료
+//          1) MongoDB shell 실행
+                `mongosh`
+//          2) admin 데이터베이스로 변경
+                `use admin`
+//          3) MongoDB 서버 종료
+                `db.shutdownServer();`
+//          4) MongoDB shell 종료
+                `exit`
+
+//      * MongoDB 재실행시 발생하는 에러
+//          shutdown 이후 mongod 입력해도 MongoDB 실행 안됨 (이유는 모름)
+//          MongoDB 재실행하려면 컴퓨터 다시 시작 하고난 후 다시 mongod 로 실행
+
+// Docker에 MongoDB 설치하기
+
+//      1. MongoDB 실행할 Dockerfile 생성
+//          1) 백엔드 폴더에서 Dockerfile.mongo 파일 생성
+//          2) Dockerfile 명령어 작성
+                `FROM mongo:5`
+//              : MongoDB 설치된 리눅스 운영체제 다운로드
+
+//          MongoDB Docker 컴퓨터 작업 끝!
+
+// Docker Compose 사용하기
+
+//      매번 Express, Mongo, ... 를 실행할 Dockerfile들을 따로따로 빌드, 실행, 
+//        종료하기 번거롭기 때문에 Docker Compose를 사용해 한번에 실행, 종료하는 방법 사용
+
+//      1. Docker Compose 설정파일 생성
+//          1) backend 폴더에 docker-compose.yaml 파일 생성
+//              확장자 yaml, yml 둘 다 사용 가능
+
+//      2. Docker Compose 설정파일 명령어 작성
+//          1) 버전 입력
+                `version: '3.7'`
+//          2) 컴퓨터 목록 입력
+                `
+                services:           // 컴퓨터 목록
+
+                  my-backend:       // 컴퓨터 이름 변수명 : 백엔드 컴퓨터
+                    build:          // 빌드에 대한 정보:
+                      context: .    // 경로명 : .(현재 위치)
+                      dockerfile: Dockerfile    // 파일 이름 : Dockerfile
+                    ports:          // 포트포워딩
+                      - 4000:4000   // 4000번 포트를 받아서 4000번 포트로 연결해라
+
+                  my-database:      // 컴퓨터 이름 변수명 : 데이터베이스 컴퓨터
+                    build:
+                      context: .
+                      dockerfile: Dockerfile.mongo // 파일 이름 : Dockerfile.mongo
+                    ports:
+                      - 27017:27017 // 27017번 포트를 받아서 27017번 포트로 연결해라
+                `
+
+//      3. 모든 Dockerfile 빌드
+//          1) 백엔드 폴더 통합 터미널 열기
+//              docker-compose.yaml 파일이 있는 위치에서 열어야 함
+//          2) Docker Compose 빌드 명령어 입력
+                `docker-compose build`
+//              docker Compose 설정파일에 등록된 모든 도커파일이 이미지로 빌드됨
+
+//      4. 모든 이미지 파일 실행
+//          1) Docker Compose 실행 명령어 입력
+                `docker-compose up`
+//              Docker Compose 설정파일에서 생성된 모든 이미지 파일이 실행됨
+//              현재 실습에서는 express, mongoDB가 설치된 이미지들이 각각 실행됨
+
+//      5. 실행중인 Docker 컴퓨터 확인하기
+//          1) 새 터미널 열어서 도커 프로세스 명령어 입력
+                `docker ps`
+//          2) 실행 상태의 컨테이너 두 개인지 확인
+//          3) NAMES (컨테이너 이름) 확인
+//              backend-my-backend-1, backend-my-database-1
+//          4) IMAGE (이미지 이름) 확인
+//              backend-my-backend, backend-my-database
+//          5) PORTS (포트 포워딩) 확인
+//              0.0.0.0:4000->4000/tcp, 0.0.0.0:27017->27017/tcp
+
+//      6. 실행중인 Docker 컴퓨터 모두 종료하기
+//          * 컴퓨터 실행된 터미널에서 종료하는 방법
+//              docker-compose up 한 터미널에서 '컨트롤 + C' 누르기
+                    `^C`
+//          * 다른 터미널에서 종료하는 방법
+//              docker-compose.yaml이 저장된 폴더의 터미널에서 종료 명령어 입력
+                    `docker-compose stop`
+
+// Docker 컴퓨터에서 MongoDB 접속하기
+
+//      1. Shell을 통한 CLI 접속
+
+//      2. MongoDB Compass를 통한 GUI 접속
+
+////////////////////////////////////////////////////
+////////////////   22:55부터 듣기   //////////////////
+////////////////////////////////////////////////////
--- memo/blog_posting.md
+++ memo/blog_posting.md
@@ -33,3 +33,40 @@ https://theworldaswillandidea.tistory.com/77
 이제 안드로이드 스튜디오에서 새로운 프로젝트를 만들 때 지정한 폴더가 기본 폴더로 사용됩니다.
 
 기본 프로젝트 폴더를 변경한 후에는 새로운 프로젝트를 만들 때 해당 폴더가 자동으로 사용됩니다. 이렇게 설정하면 매번 프로젝트를 만들 때마다 기본 경로를 수동으로 변경할 필요가 없습니다.
+
+4. 윈도우에서 MongoDB 종료시 재시작 안되는 에러
+
+MongoDB 종료 후 에러 발생까지의 과정
+- 윈도우 파워쉘에서 mongod 명령어로 MongoDB 실행
+- mongosh 명령어로 MongoDB Shell 실행
+- use admin 명령어로 admin db로 변경
+- db.shutdownServer(); 명령어로 MongoDB 종료
+- exit 명령어로 MongoDB shell 종료
+- 이후 mongosh 로 MongoDB가 종료되어 실행 안되는 것 확인
+- 근데 여기서 mongod 로 MongoDB를 실행하고 mongosh 로 MongoDB Shell 을 열었으나 connection error? 이런 에러 뜸 나중에 다시 테스트
+
+시도한 방법들
+
+- mongod.lock 파일 삭제
+-> MongoDB가 비정상 종료되었을 경우 이를 해결하는 작업을 다음 MongoDB 실행때 수행하는데 수행할 작업을 mongod.lock에 적어둔다고 함
+-> 이 작업 자체가 잘못 적혀있거나 수행되며 에러가 발생할 수도 있어서 lock 파일을 지우면 해결될 수도 있다고 함
+-> 지웠으나 동일한 에러 발생
+
+- 작업관리자 - 서비스에서 MongoDB 상태 확인하기
+-> 상태 정지 로 되어있음
+-> 우클릭해서 시작 눌러도 잠깐 바뀌었다가 다시 정지됨
+-> 서비스 속성에서는 사용/사용안함/자동 중 자동으로 되어있음
+-> 서비스 시작 눌러도 시작 안되고 에러 뜸
+
+- mongod --dbpath D:Apps/MongoDB/.../data 명령어 실행
+-> 위 명령어 입력한 powershell에서는 더이상 입력 안됨
+-> 다른 탭에서 mongosh 정상 작동
+-> mongod --dbpath ... 입력한 파워쉘의 작업을 ^C로 끝내거나 탭을 닫으면 MongoDB 서버도 닫힘
+-> mongod 실행한 파워쉘 닫은 후에는 mongosh 나 MongoDB Compass 둘 다 connection 실패
+
+- 컴퓨터 다시시작 하기
+-> 컴퓨터 재시작 후 파워쉘에서 mongod, mongosh 명령어 수행
+-> 정상 작동함(???)
+-> 다시 db.shutdownServer();로 MongoDB 종료 후 재시작하니 똑같은 에러 발생
+-> 다시 컴퓨터 껏다 켜니 정상 동작
+-> shutdown 이후 왜 다시 안켜지는지 나중에 구글링해보기
