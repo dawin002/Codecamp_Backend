856541fdff7c81ae632f588def47651760b97792
dawin002 <dawin002@naver.com>
2024-03-12 23:22:51 +0900

    study(09_NestJS): eslint prettier

--- 09_Nest.js/class/02-eslint-prettier/src/app.controller.ts
+++ 09_Nest.js/class/02-eslint-prettier/src/app.controller.ts
@@ -10,12 +10,14 @@ export class AppController {
     // const qqq = 3; // 원래 qqq에 eslint 에러(노란줄, 선언은 되었으나 사용되지 않았음) 떠야함
 
     const qqq = 3;
+    qqq;
 
     let a = 0;
-
+    a = 1;
     console.log(a);
 
     const abc = 'asdf';
+    abc;
 
     return this.appService.getHello();
   }
