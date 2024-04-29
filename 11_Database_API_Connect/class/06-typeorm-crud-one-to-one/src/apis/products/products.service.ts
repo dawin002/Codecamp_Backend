57704ab3efe889334254a0bec913fa40874b516d
dawin002 <dawin002@naver.com>
2024-04-29 17:37:45 +0900

    study(11_Database_API_Connect): TypeORM CRUD - one to one

---
+++ 11_Database_API_Connect/class/06-typeorm-crud-one-to-one/src/apis/products/products.service.ts
@@ -0,0 +1,75 @@
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  IProductsServiceCheckSoldout,
+  IProductsServiceDelete,
+  IProductsServiceFindOne,
+  IProductsServiceUpdate,
+  IProductsSeviceCreate,
+} from './interfaces/products-service.interface';
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>, //
+  ) {}
+
+  findAll(): Promise<Product[]> {
+    return this.productsRepository.find();
+  }
+
+  findOne({ productId }: IProductsServiceFindOne): Promise<Product> {
+    return this.productsRepository.findOne({ where: { id: productId } });
+  }
+
+  create({ createProductInput }: IProductsSeviceCreate): Promise<Product> {
+    const result = this.productsRepository.save({
+      ...createProductInput,
+    });
+
+    return result;
+  }
+
+  async update({
+    productId,
+    updateProductInput,
+  }: IProductsServiceUpdate): Promise<Product> {
+    const product = await this.findOne({ productId });
+
+    this.checkSoldout({ product });
+
+    const result = this.productsRepository.save({
+      ...product,
+      ...updateProductInput,
+    });
+
+    return result;
+  }
+
+  checkSoldout({ product }: IProductsServiceCheckSoldout): void {
+    if (product.isSoldout) {
+      throw new UnprocessableEntityException('이미 핀매 완료된 상품입니다.');
+    }
+  }
+
+  async delete({ productId }: IProductsServiceDelete): Promise<boolean> {
+    // 1. 실제 삭제
+    // const result = await this.productsRepository.delete({ id: productId });
+
+    // 2. 소프트 삭제(직접 구현) - isDeleted 칼럼
+    // this.productsRepository.update({ id: productId }, { isDeleted: true });
+
+    // 3. 소프트 삭제(직접 구현) - deletedAt 칼럼
+    // this.productsRepository.update({id: productId}, {deletedAt: new Date() });
+
+    // 4. 소프트 삭제(TypeORM 제공) - softRemove
+    // this.productsRepository.softRemove({ id: productId });
+
+    // 5. 소프트 삭제(TypeORM 제공) - softDelete
+    const result = await this.productsRepository.softDelete({ id: productId });
+    return result.affected ? true : false;
+  }
+}
