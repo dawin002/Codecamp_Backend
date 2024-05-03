ed0ea5af20ebb5928384f56662ffebf28373ee2c
dawin002 <dawin002@naver.com>
2024-05-04 02:46:59 +0900

    study(12_Login): Login api authentication

---
+++ 12_Login/class/01-signup copy/src/apis/users/entities/user.entity.ts
@@ -0,0 +1,26 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => String)
+  id: string;
+
+  @Column()
+  @Field(() => String)
+  email: string;
+
+  @Column()
+  // @Field(() => String) // 비밀번호는 브라우저에 전달하면 안됨
+  password: string;
+
+  @Column()
+  @Field(() => String)
+  name: string;
+
+  @Column()
+  @Field(() => Int)
+  age: number;
+}
