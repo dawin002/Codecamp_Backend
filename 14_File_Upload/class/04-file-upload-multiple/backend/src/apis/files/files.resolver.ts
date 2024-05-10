9faa4eca53fcaa886fd012abbf6da0911fbef46d
dawin002 <dawin002@naver.com>
2024-05-10 22:40:08 +0900

    study(14_File_Upload): multiple image upload

--- 14_File_Upload/class/04-file-upload-multiple/backend/src/apis/files/files.resolver.ts
+++ 14_File_Upload/class/04-file-upload-multiple/backend/src/apis/files/files.resolver.ts
@@ -12,7 +12,7 @@ export class FilesResolver {
   uploadFile(
     // 브라우저에서 파일 받아오기
     @Args({ name: 'file', type: () => GraphQLUpload }) file: FileUpload, // gql 타입과 ts 타입 다름
-  ): Promise<string> {
+  ): string {
     // 받은 파일 upload 서비스 함수로 전달하기
     return this.filesService.upload({ file });
   }
--- 14_File_Upload/class/04-file-upload-multiple/backend/src/apis/files/files.service.ts
+++ 14_File_Upload/class/04-file-upload-multiple/backend/src/apis/files/files.service.ts
@@ -4,7 +4,7 @@ import { IFilesServiceUpload } from './interfaces/files-service.interface';
 
 @Injectable()
 export class FilesService {
-  async upload({ file }: IFilesServiceUpload): Promise<string> {
+  upload({ file }: IFilesServiceUpload): string {
     console.log(file);
 
     // 1. 파일을 클라우드 스토리지에 저장하기
@@ -16,20 +16,15 @@ export class FilesService {
     }).bucket('codecamp-file-storage-dawin002'); // gcp 프로젝트의 사용할 버킷 등록
 
     // 1-2) 스토리지에 파일 올리기
-
-    await new Promise((resolve, reject) => {
-      file
-        .createReadStream()
-        .pipe(storage.file(file.filename).createWriteStream())
-        .on('finish', () => {
-          console.log('성공');
-          resolve('성공');
-        })
-        .on('error', () => {
-          console.log('실패');
-          reject('실패');
-        });
-    });
+    file
+      .createReadStream()
+      .pipe(storage.file(file.filename).createWriteStream())
+      .on('finish', () => {
+        console.log('성공');
+      })
+      .on('error', () => {
+        console.log('실패');
+      });
 
     console.log('파일 전송이 완료되었습니다.');
 
